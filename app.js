3351d37be33352378cac750e36dce298faeb1acf
rahul1237 <rahulmahajan98645@gmail.com>
2021-03-07 22:34:46 +0530

    added

--- app.js
+++ app.js
@@ -1,29 +1,5 @@
-// file system!
+const validator = require('validator')
+const notes = require('./notes')
 
-// we have to require the module and convert it to the object!
-const filesystem = require('fs')
-
-// creatting a new file and adding data to it!
-filesystem.writeFileSync('notes.txt','my name is rahul mahajan')
-
-// appending the data to the existing file!
-filesystem.appendFileSync('notes.txt','. again here to lear node from scratch')
-
-const name = 'rahul'
-
-// here we are importing utils.js file into our main file APP.JS
-const add = require('./utils')
-
-console.log(name)
-
-// by exporting objects from the utils.js file we are using them in our main file!
-console.log(add(2,3));
-
-const note = require('./notes')
-
-msg = note()
-
-console.log(msg);
-
-
-// MAIN PROJECT!
+console.log(validator.isEmail('rahulmahajan98645@gmail.com')); 
+console.log(notes());
